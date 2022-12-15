9a9c114ec5a22c59d090e1dc7c75febd6bea17e5
Bao Nguyen <ntbao.me@gmail.com>
2022-12-16 00:35:11 +0700

    use turbo stream when submit form and render partial

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -25,6 +25,9 @@ class PostsController < ApplicationController
 
     respond_to do |format|
       if @post.save
+        format.turbo_stream {
+          render turbo_stream: turbo_stream.prepend('posts', partial: 'posts/post', locals: { post: @post})
+        }
         format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
         format.json { render :show, status: :created, location: @post }
       else
